dd8cad7033d41527f05547f6baa37f9085524c0e
Satori Satori <3102809947@qq.com>
2022-10-07 13:47:58 +0800

    upd:game

---
+++ src/main/java/cn/edu/sustech/cs309/domain/Game.java
@@ -0,0 +1,41 @@
+package cn.edu.sustech.cs309.domain;
+
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "game")
+@EntityListeners(AuditingEntityListener.class)
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createTime;
+
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updateTime;
+
+    //need foreign key ?
+    private Integer player1id;
+
+    private Integer player2id;
+
+    //false means player1 first,otherwise player2 first
+    private Boolean playerFirst;
+}
