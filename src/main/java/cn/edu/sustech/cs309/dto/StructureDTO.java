9c85bf6c2acca8d825a5d9bfb65c4e19d94af2af
Tavish <12011618@mail.sustech.edu.cn>
2022-10-24 00:20:52 +0800

    update structure

--- src/main/java/cn/edu/sustech/cs309/dto/StructureDTO.java
+++ src/main/java/cn/edu/sustech/cs309/dto/StructureDTO.java
@@ -1,11 +1,10 @@
 package cn.edu.sustech.cs309.dto;
 
-import cn.edu.sustech.cs309.domain.CharacterClass;
-import cn.edu.sustech.cs309.domain.Player;
 import cn.edu.sustech.cs309.domain.StructureClass;
-import cn.edu.sustech.cs309.domain.StructureRecord;
 
 import java.io.Serializable;
+import java.util.List;
 
-public record StructureDTO(Integer id, PlayerDTO playerDTO, StructureClass structureClass,Integer level,Integer hp,Integer remainingRound,Integer value,Integer x,Integer y,String character) implements Serializable {
+public record StructureDTO(Integer id, StructureClass structureClass, Integer level, Integer hp, Integer remainingRound,
+                           Integer value, Integer x, Integer y, List<CharacterDTO> characterDTOS) implements Serializable {
 }
--- src/main/java/cn/edu/sustech/cs309/repository/StructureRecordRepository.java
+++ src/main/java/cn/edu/sustech/cs309/repository/StructureRecordRepository.java
@@ -1,10 +1,17 @@
 package cn.edu.sustech.cs309.repository;
 
+import cn.edu.sustech.cs309.domain.Player;
 import cn.edu.sustech.cs309.domain.StructureRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StructureRecordRepository extends JpaRepository<StructureRecord, Integer> {
     StructureRecord findStructureRecordById(Integer id);
+
+    @Query(value = "select sum(level) from structure_record where player_id = ?1", nativeQuery = true)
+    Integer getSumLevel(Integer playerId);
+
+    Integer countByPlayer(Player player);
 }
