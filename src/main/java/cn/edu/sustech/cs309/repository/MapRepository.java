b81ac9d68559679c8d2b6871dedcbaf45efc7663
Tavish <12011618@mail.sustech.edu.cn>
2022-11-12 09:25:02 +0800

    add MapRepository

---
+++ src/main/java/cn/edu/sustech/cs309/repository/MapRepository.java
@@ -0,0 +1,12 @@
+package cn.edu.sustech.cs309.repository;
+
+import cn.edu.sustech.cs309.domain.Map;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface MapRepository extends JpaRepository<Map, Integer> {
+    Map findMapById(Integer id);
+
+    @Query(value = "select count(*) from map", nativeQuery = true)
+    int countAll();
+}
