15cc1a04bbeba7c5e5568081eaa212bd63e9b9f2
Satori <3102809947@qq.com>
2022-11-25 11:54:22 +0800

    add class basic

--- src/main/java/cn/edu/sustech/cs309/domain/EquipmentClass.java
+++ src/main/java/cn/edu/sustech/cs309/domain/EquipmentClass.java
@@ -1,6 +1,7 @@
 package cn.edu.sustech.cs309.domain;
 
 public enum EquipmentClass {
+    BASIC,
     SWORD,
     ARROW,
     CANNON,
--- src/main/java/cn/edu/sustech/cs309/domain/MountClass.java
+++ src/main/java/cn/edu/sustech/cs309/domain/MountClass.java
@@ -1,6 +1,7 @@
 package cn.edu.sustech.cs309.domain;
 
 public enum MountClass {
+    BASIC,
     FOX,
     HORSE,
     ELEPHANT,
